b90b4751867550fea4ad4a048a5c462b731ca6b3
diwahar-m <msdiwahar5@gmail.com>
2025-06-25 21:54:41 +0530

    initiated redux for travel state

--- App.tsx
+++ App.tsx
@@ -7,15 +7,19 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NewAppScreen templateFileName="App.tsx" />
+      </View>
+    </Provider>
   );
 }
 
